e5cfbd2cf8435a9447c78db89a99b275ea62a86f
rachael <rachael@purplellamas.net>
2023-11-30 19:26:52 -0800

    Get a new project set up and ready to go
    new xcode proj, and some basic code up so the test input can be put in & ready to go!

---
+++ 2023/AoC2023/AoC2023/Day1/day1.swift
@@ -0,0 +1,21 @@
+//
+//  day1.swift
+//  AoC2023
+//
+//  Created by Rachael Worthington on 11/30/23.
+//
+
+import Foundation
+
+
+func day1() {
+    let path = "/Users/rachael/Documents/AoC-remote/2023/AoC2023/AoC2023/Day1/input"
+    do {
+        let text = try String(contentsOfFile: path, encoding: .utf8)
+        
+        // do something interesting
+
+    } catch {
+        print(error.localizedDescription)
+    }
+}
---
+++ 2023/AoC2023/AoC2023/main.swift
@@ -0,0 +1,10 @@
+//
+//  main.swift
+//  AoC2023
+//
+//  Created by Rachael Worthington on 11/30/23.
+//
+
+import Foundation
+
+day1()
