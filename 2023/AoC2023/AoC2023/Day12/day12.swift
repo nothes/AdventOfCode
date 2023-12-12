c29eaf13c83294d2058a8cd3121bf3cabfc651c7
rachael <rachael@purplellamas.net>
2023-12-11 22:35:19 -0800

    further contemplations on matching groups of numbers
    I think keeping track of pairs of broken and operational springs is the way to go, but I'm not sure where to go from there.

--- 2023/AoC2023/AoC2023/Day12/day12.swift
+++ 2023/AoC2023/AoC2023/Day12/day12.swift
@@ -27,12 +27,8 @@ func parseRecords(with input: String) {
     input.enumerateLines { line, stop in
         if line.isEmpty == false { // stop forgetting this.
             let parts = line.split(separator: /\s+/)
-            var states: [SpringState] = []
-            for char in parts[0] {
-                states.append(SpringState(rawValue: String(char))!)
-            }
             let counts = parts[1].split(separator: ",").map { Int($0)! }
-            records.append(Record(diagram: states, counts: counts))
+            records.append(Record(diagram: String(parts[0]), counts: counts))
         }
     }
 }
@@ -63,52 +59,84 @@ struct StateGroup: CustomDebugStringConvertible {
     }
 }
 
-struct Record {
-    let diagram: [SpringState]
-    let counts: [Int]
+struct SpringGroup {
+    let broken: Int // must be 1 or more
+    let operational: Int // could be 0 in the last one, but otherwise must be 1 or more.
+    let unknown: Int // could be 0
+}
 
-    func stateGroups() -> [StateGroup] {
-        var stateGroups: [StateGroup] = []// type, and number
+// THE ALGORITHM: Each spring table can be described as a pair of broken, operational springs, with a possible leading . count. for example "#.#.###" could be 0, [1,1][1,1][3,0], and the first numbers (the brokens) match the number array at the end of the input
+// so we know how many sections there have to be. and we can matxh the springs with them.
+//
+// ????.######..#####. 1,6,5
+// ? [1,?][6,?][5,?]
 
-        var currentGroupType = diagram[0]
-        var currentCount = 1
+// comparing that with our chart, we can fill in the values, beaxuse we can find the last 2 groups
+// ? [1,(at least 1)][6,2][5,1]
+// so our only question is "how many places can 1 spring fit in the first 4?" which is 4.
 
-        let diagram = diagram
-        for i in 1..<diagram.count {
-            let state = diagram[i]
-            if state == currentGroupType {
-                currentCount = currentCount + 1
-            } else {
-                stateGroups.append(StateGroup(state: currentGroupType, count: currentCount))
-                currentGroupType = state
-                currentCount = 1
+// ?###???????? 3,2,1
+// ?[3,?][2,?][1,?]
+// so we know from our diagram that
+// 1[3,(at least 1)][2,(at least 1)][1,(at least 0)]
+// so our question is how to get 2 pairs into the last 7
+
+class Record: CustomDebugStringConvertible {
+    var debugDescription: String {
+        return "input: \(diagram) \(counts)\nknownInfo: \(initialWorkingSprings) \(springPairCounts)"
+    }
+
+    let diagram: String // read in info
+    let counts: [Int] // read in info
+
+    // throughout this -1 = unknown
+    var initialWorkingSprings = -1 // this could be 0
+    var springPairCounts: [(broken: Int, working: Int)] = [] // these count should match the counts array
+
+    init(diagram: String, counts: [Int]) {
+        self.diagram = diagram
+        self.counts = counts
+
+        for count in counts {
+            springPairCounts.append((count, -1))
+        }
+        generatePartialSolution()
+    }
+
+    func generatePartialSolution() {
+        // this fills in as many -1's as we can.
+
+        //first of all, can we establish a leading-working-spring count?
+        let workingSprings = diagram.prefix { char in
+            char == "."
+        }
+        initialWorkingSprings = workingSprings.count
+
+        var count = 0
+        if diagram.last == "." {
+            for char in diagram.reversed() {
+                if char == "." {
+                    count = count + 1
+                } else {
+                    break
+                }
+            }
+            var newLast = springPairCounts.last!
+            if let last = springPairCounts.popLast() {
+                newLast = (last.broken, count)
+                springPairCounts.append(newLast)
+            }
+        } else if diagram.last == "#" {
+            if let last = springPairCounts.popLast() {
+                let newLast = (last.broken, 0)
+                springPairCounts.append(newLast)
             }
         }
-        stateGroups.append(StateGroup(state: currentGroupType, count: currentCount))
-
-        return stateGroups
     }
 }
 
 func findSolutionCombos() {
     for record in records {
-      //  let groupsOfBustedSprings = record.counts.count
-        let stateGroups = record.stateGroups()
-        print("busted counts: \(record.counts)")
-        print("current groups: \(stateGroups)")
-
-        // lets see if we can match up the counts with our groups
-        var matches: [(StateGroup, Int)] = [] // group and the index of the springCountArray it matches to
-
-        var currentCountIndex = 0
-        for state in stateGroups {
-            let count = record.counts[currentCountIndex]
-            print("\(state) vs \(count) (index: \(currentCountIndex))")
-            if state.state == .broken && state.count == count { // these match up. yay!
-                matches.append((state, currentCountIndex))
-                currentCountIndex = currentCountIndex + 1
-            }
-        }
-        print("these go together, maybe: \(matches)")
+        print(record)
     }
 }
