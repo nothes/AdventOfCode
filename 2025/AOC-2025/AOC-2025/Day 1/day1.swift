c369d9eb0bf70c8f04b79d4791bb5f48a19e27ca
rachael <rachael@omnigroup.com>
2025-11-30 21:34:34 -0800

    Day 1 solutions
    that spin multiple times thing was sly.

--- 2025/AOC-2025/AOC-2025/Day 1/day1.swift
+++ 2025/AOC-2025/AOC-2025/Day 1/day1.swift
@@ -9,5 +9,44 @@ import Foundation
 
 func day1()
 {
-    
+    let input = readInput(forDay: 1)
+    var currentDigit = 50
+    var zeroCount = 0
+
+    let actions = input.split(separator: "\n")
+    for act in actions {
+        var action = act
+        let dir = action.removeFirst()
+        let result = Int(action)!.quotientAndRemainder(dividingBy: 100)
+        let movement = result.remainder
+        let quotient = result.quotient
+        zeroCount += quotient
+        if dir == "L" {
+            if currentDigit > movement {
+                currentDigit = currentDigit - movement
+            } else {
+                let oldCurrDig = currentDigit
+                currentDigit = 100 - (movement - currentDigit)
+                if currentDigit == 100 {
+                    currentDigit = 0
+                }
+                if oldCurrDig != 0 {
+                    zeroCount += 1
+                }
+            }
+        } else if dir == "R" {
+            if currentDigit + movement <= 99 {
+                currentDigit = currentDigit + movement
+            } else {
+                let oldCurrDig = currentDigit
+                currentDigit = movement - (100 - currentDigit)
+                if oldCurrDig != 0 {
+                    zeroCount += 1
+                }
+            }
+        }
+    }
+
+    print("zero total: \(zeroCount)")
+
 }
