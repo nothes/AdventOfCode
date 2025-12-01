7a9d58a733b259b0b21c5912352ca8224227f688
rachael <rachael@omnigroup.com>
2025-11-30 20:32:03 -0800

    Prep for Day one of 2025! here we go!

---
+++ 2025/AOC-2025/AOC-2025/Day 1/day1.swift
@@ -0,0 +1,13 @@
+//
+//  day1.swift
+//  AOC-2025
+//
+//  Created by Rachael Worthington on 11/30/25.
+//
+
+import Foundation
+
+func day1()
+{
+    
+}
---
+++ 2025/AOC-2025/AOC-2025/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  AOC-2025
+//
+//  Created by Rachael Worthington on 11/30/25.
+//
+
+import Foundation
+
+
+//day1()
+
+func readInput(forDay day: Int) -> String {
+    let path = "/Users/rachael/Documents/AoC-remote/2025/AOC-2025/AOC-2025/Day \(day)/input.txt"
+    do {
+        let text = try String(contentsOfFile: path, encoding: .utf8)
+        return text
+    } catch {
+        print(error.localizedDescription)
+    }
+    return ""
+}
